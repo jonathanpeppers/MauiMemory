443eabd670fb681460167ef00e0dc54e234252dc
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-06-04 13:52:49 -0500

    Add GC.Collect() calls
    This is just for debugging purposes, don't put these in your app

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,22 +16,31 @@ namespace MauiMemory
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
             var totalMemory = GC.GetTotalMemory(true);
             memTxt.Text = $"Memory: {totalMemory}";
         }
 
         private async void CounterBtn_Clicked(object sender, EventArgs e)
         {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
             await _navigation.navigateToAsync(NavigationService.Page1);
         }
 
         private async void CounterBtn2_Clicked(object sender, EventArgs e)
         {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
             await _navigation.navigateToAsync(NavigationService.Page2);
         }
 
         private async void CounterBtn3_Clicked(object sender, EventArgs e)
         {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
             await _navigation.navigateToAsync(NavigationService.Page3);
         }
     }
